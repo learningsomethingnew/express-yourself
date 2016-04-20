e42b1981da2afdac3bb262722bef75faa68072ee
Nic Colley <nic.colley@live.com>
2016-04-20 08:14:24 -0400

    hex done

--- validator.py
+++ validator.py
@@ -7,4 +7,17 @@ def binary(a_string):
     p = re.compile(r'\b[01]+\b')
     return re.match(p, a_string)
 
-print(binary(10))
+def binary_even(a_string):
+    if binary:
+        temp_int = int(a_string)
+        if temp_int % 2 == 0:
+            return True
+        else:
+            return False
+
+def hex(a_string):
+    p = re.compile(r'\b[0-9A-F]+\b')
+    print(a_string)
+    return re.match(p, a_string)
+
+print(hex("CAFE"))
